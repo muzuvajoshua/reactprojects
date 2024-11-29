e00096953839f97d9029cf6ab628c2f6e0020870
muzuvajoshua <muzuvajoshua@gmail.com>
2024-11-29 10:36:50 +0300

    added a modal component, a form inside the modal component and also put functionality to edit content from the parent component

--- src/App.js
+++ src/App.js
@@ -1,42 +1,61 @@
 import './App.css';
 import Employee from './components/Employee';
 import { useState } from 'react';
-import {v4 as uuidv4} from 'uuid';
+// import {v4 as uuidv4} from 'uuid';
 
 function App() {
   const [role ,setRole] =useState('Software engineer');
   const [employees, setEmployee] = useState([
     {
+      id:1,
       name:"Joshua",
       role:"Software Engineer",
       img:"https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg"
     },
     {
+      id:2,
       name:"Joshua",
       role:"Software Engineer",
       img:"https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg"
     },
     {
+      id:3,
       name:"Joshua",
       role:"Software Engineer",
       img:"https://images.pexels.com/photos/2169434/pexels-photo-2169434.jpeg"
     },
     {
+      id:4,
       name:"Joshua",
       role:"Software Engineer",
       img:"https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg"
     },
     {
+      id:5,
       name:"Joshua",
       role:"Software Engineer",
       img:"https://images.pexels.com/photos/2104252/pexels-photo-2104252.jpeg"
     },
     {
+      id:6,
       name:"Joshua",
       role:"Software Engineer",
       img:"https://images.pexels.com/photos/1844547/pexels-photo-1844547.jpeg"
     }
   ]);
+
+  function updateEmployee(id, newName, newRole){
+    const updatedEmployees = employees.map((employee) =>{
+      if(id === employee.id){
+        return{...employee, name:newName, role:newRole}
+
+      }
+      return employee;
+
+    });
+    setEmployee(updatedEmployees)
+   
+  }
   return (
     <div className="App">
 
@@ -44,10 +63,12 @@ function App() {
         <div className='flex flex-wrap justify-center'>
         {employees.map((employee)=> {
           return (<Employee
-          key ={uuidv4()}
+          key ={employee.id}
+          id={employee.id}
           name ={employee.name}
           role={employee.role}
           img={employee.img}
+          updateEmployee={updateEmployee}
           />
           );
 
